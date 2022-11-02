d5d84cd8a521347cffeb0fbbc4b0d5a05b3fa27c
Aliaksei Kasiuk <aliakseikasiuk@gmail.com>
2022-11-02 16:41:12 +0300

    feat: add copyDir function to create a deep copy files from 'files' directory for the '04-copy-directory' task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,164 @@
+const path = require('path');
+const fs = require('fs');
+const root = __dirname;
+const fromDirFullPath = path.join(root, 'files');
+const toDirFullPath = path.join(root, 'files-copy');
 
+function copyDir(src, dest) {
+  // PROMISES___HELL
+
+  // // if copy already exist? => remove it and create new empty dir to copy
+  // fs.promises
+  //   .readdir(to)
+  //   .then(() => {
+  //     console.log('old copy exist');
+  //     fs.promises
+  //       .rm(to, { recursive: true })
+  //       .then(() => {
+  //         console.log('old copy removed');
+  //         fs.promises
+  //           .mkdir(to)
+  //           .then(() => {
+  //             console.log('create new dir for copy');
+  //             fs.promises
+  //               .readdir(from)
+  //               .then((files) => {
+  //                 files.forEach((file) => {
+  //                   const fromFileFullPath = path.join(from, file);
+  //                   const toFileFullPath = path.join(to, file);
+  //                   fs.promises
+  //                     .stat(fromFileFullPath)
+  //                     .then((stats) => {
+  //                       if (stats.isFile()) {
+  //                         // console.log(`${file} - file`);
+  //                         fs.promises
+  //                           .readFile(fromFileFullPath)
+  //                           .then((data) => {
+  //                             console.log(toFileFullPath);
+  //                             fs.promises
+  //                               .writeFile(toFileFullPath, data)
+  //                               .then((isWrite) => {
+  //                                 if (isWrite == undefined)
+  //                                   console.log(`${file} copied`);
+  //                               })
+  //                               .catch((e) => console.error(e));
+  //                           })
+  //                           .catch((e) => console.error(e));
+  //                       } else if (stats.isDirectory()) {
+  //                         // console.log(`${file} - directory`);
+  //                         copyDir(fromFileFullPath, toFileFullPath);
+  //                       }
+  //                     })
+  //                     .catch((e) => console.error(e));
+  //                 });
+  //               })
+  //               .catch((e) => console.error(e));
+  //           })
+  //           .catch((e) => console.error(e));
+  //       })
+  //       .catch((e) => console.error(e));
+  //   })
+  //   .catch(() => {
+  //     console.log("old copy doesn't exist");
+  //     fs.promises
+  //       .mkdir(to)
+  //       .then(() => {
+  //         console.log('create dir for copy');
+  //         fs.promises
+  //           .readdir(from)
+  //           .then((files) => {
+  //             files.forEach((file) => {
+  //               const fromFileFullPath = path.join(from, file);
+  //               const toFileFullPath = path.join(to, file);
+  //               fs.promises
+  //                 .stat(fromFileFullPath)
+  //                 .then((stats) => {
+  //                   if (stats.isFile()) {
+  //                     // console.log(`${file} - file`);
+  //                     fs.promises
+  //                       .readFile(fromFileFullPath)
+  //                       .then((data) => {
+  //                         console.log(toFileFullPath);
+  //                         fs.promises
+  //                           .writeFile(toFileFullPath, data)
+  //                           .then((isWrite) => {
+  //                             if (isWrite == undefined)
+  //                               console.log(`${file} copied`);
+  //                           })
+  //                           .catch((e) => console.error(e));
+  //                       })
+  //                       .catch((e) => console.error(e));
+  //                   } else if (stats.isDirectory()) {
+  //                     // console.log(`${file} - directory`);
+  //                     copyDir(fromFileFullPath, toFileFullPath);
+  //                   }
+  //                 })
+  //                 .catch((e) => console.error(e));
+  //             });
+  //           })
+  //           .catch((e) => console.error(e));
+  //       })
+  //       .catch((e) => console.error(e));
+  //   });
+  // prepare to copy: remove old copy-dir, then call copyFiles function;
+
+  fs.promises
+    .readdir(dest)
+    .then(() => {
+      //dest exists. let's remove it with all children files;
+      fs.promises
+        .rm(dest, { recursive: true })
+        .then(() => {
+          // let's create new dest directory and copy files
+          copyDirChildren(src, dest);
+        })
+        .catch((e) => console.error(e));
+    })
+    .catch(() => {
+      // dest doesn't exist.
+      // let's create dest directory and copy files
+      copyDirChildren(src, dest);
+    });
+}
+function copyDirChildren(src, dest) {
+  // create dest dir for copy
+  fs.promises
+    .mkdir(dest)
+    .then(() => {
+      // get all names for all files and dirs from src
+      fs.promises
+        .readdir(src)
+        .then((files) => {
+          files.forEach((file) => {
+            const fromFileFullPath = path.join(src, file);
+            const toFileFullPath = path.join(dest, file);
+
+            // get info about each children from src
+            fs.promises
+              .stat(fromFileFullPath)
+              .then((stats) => {
+                // if it's a file => let's read it for copy
+                if (stats.isFile()) {
+                  fs.promises
+                    .readFile(fromFileFullPath)
+                    .then((data) => {
+                      // write to copy-file in dest
+                      fs.promises
+                        .writeFile(toFileFullPath, data)
+                        .catch((e) => console.error(e));
+                    })
+                    .catch((e) => console.error(e));
+                } else if (stats.isDirectory()) {
+                  // if it's directory => recursive call this func with new src & dest
+                  copyDirChildren(fromFileFullPath, toFileFullPath);
+                }
+              })
+              .catch((e) => console.error(e));
+          });
+        })
+        .catch((e) => console.error(e));
+    })
+    .catch((e) => console.err(e));
+}
+
+copyDir(fromDirFullPath, toDirFullPath);
