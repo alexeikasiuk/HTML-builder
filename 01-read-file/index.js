fbd280b364e8ffe3127049d889eeabcce08c8a45
Aliaksei Kasiuk <aliakseikasiuk@gmail.com>
2022-11-03 14:56:02 +0300

    fix: remove aditional featchers '01-read-file' task according cross-check-list

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const url = path.resolve(__dirname, 'text.txt');
+const readStream = fs.createReadStream(url, 'utf8');
 
-console.log("Let's try to read text file!\n");
-
-const readStream = fs.createReadStream(url);
-
-readStream.on('data', (chunk) => console.log(chunk.toString()));
-readStream.on('error', () => console.log("Error!!! File doesn't exist!!!"));
-readStream.on('end', () => console.log('\nFinished reading!'));
+readStream.on('data', (text) => console.log(text));
